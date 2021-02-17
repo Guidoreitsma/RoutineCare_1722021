0f260633710a27a0fbe6625b99efb0694ce8f3d4
Guidoreitsma <g.reitsma@students.uu.nl>
2021-02-17 14:32:47 +0100

    Update Step 1+2.R

--- Step 1+2.R
+++ Step 1+2.R
@@ -35,9 +35,9 @@ merged_set <- merge(data_persons, minIday_OP_followup, all.x=T)
 #STEP 4 Creating a data table with the Diagnosis event codes and vocabulary
 
 Vocabulary = data.table(
-  Diagnosis = c("GBS", "GBS"),
-  event_code = c("357.0", "G61.0"),
-  vocabulary = c("ICD9", "ICD10")
+  Diagnosis = c("GBS", "GBS", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "narrow", "possible", "possible", "possible", "possible", "possible", "possible", "possible", "possible", "possible", "possible", "possible", "possible", "possible", "possible", "possible", "possible", "possible", "possible", "possible", "possible"),
+  event_code = c("357.0", "G61.0", "999.4", "T80.5", "T88.6", "T78.00", "995.60", "T78.05", "995.64", "T78.0", "995.5", "995.6", "T78.01", "995.61", "995.62", "T78.04", "995.63", "995.65", "T78.06", "995.66", "995.67", "T78.08", "995.68", "T78.02", "T78.03", "T78.07", "T78.09", "T78.2XXA", "T78.2XXD", "T78.2XXS", "995.0", "995.69", "T78.3", "995.1", "R23.0", "782.5", "R60.9", "782.3", "T78.4", "T78.40", "I95", "I95.9", "458", "458.9", "R57.9", "785.50", "L50.0", "708.0", "995.3", "995.4", "995.27", "995.2"),
+  vocabulary = c("ICD9", "ICD10/CM", "ICD9", "ICD10/CM", "ICD10/CM", "ICD10/CM", "ICD9", "ICD10/CM", "ICD9", "ICD10/CM", "ICD9", "ICD9", "ICD10/CM", "ICD9", "ICD9", "ICD10/CM", "ICD9", "ICD9", "ICD10/CM", "ICD9", "ICD9", "ICD10/CM", "ICD9", "ICD10/CM", "ICD10/CM", "ICD10/CM", "ICD10/CM", "ICD10/CM", "ICD10/CM", "ICD10/CM", "ICD9", "ICD9", "ICD10/CM", "ICD9", "ICD10/CM", "ICD9", "ICD10/CM", "ICD9", "ICD10/CM", "ICD10/CM", "ICD10/CM", "ICD10/CM", "ICD9", "ICD9", "ICD10/CM", "ICD9", "ICD10/CM", "ICD9", "ICD9", "ICD9", "ICD9", "ICD9")
 )
 
 
@@ -47,10 +47,14 @@ Vocabulary = data.table(
 EVENTS<-fread("EVENTS.csv")
 EVENTS
 merge_events <- merge(EVENTS, Vocabulary, all.x=T)
-merge_events[, .N, by=.(Diagnosis)] #Count by different diagnoses, add entries in the datatable, to get a better count.
+merge_events[, .N, by=.(Diagnosis)] #Count by different diagnoses, add entries in the data table, to get a better count.
 
-merge_events<-merge_events[Diagnosis=="GBS"]
+#merge_events<-merge_events[Diagnosis=="GBS"]
+merge_events<-merge_events[Diagnosis=="possible"]
 merge_events<-merge_events[start_date_record>20169999]
+merge_events<-unique(merge_events[start_date_record<20210000])
+merge_events<-unique(merge_events$person_id)
+merge_events[, .N, by=.(meaning_of_event)]
 merge_events
 
 #Step6
